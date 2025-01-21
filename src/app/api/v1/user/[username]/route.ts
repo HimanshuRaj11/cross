697172f4222a58560f312c2ee7d561cc7432a004
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-01-21 19:04:08 +0530

    commit

--- src/app/api/v1/user/[username]/route.ts
+++ src/app/api/v1/user/[username]/route.ts
@@ -2,9 +2,9 @@ import { InternalServerError } from "@/lib/handleError";
 import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
-export async function GET(requesr: Request, { params }: { params: { username: string } }) {
+export async function GET(request: Request, { params }: { params: Promise<{ username: string }> }) {
     try {
-        const { username } = await params;
+        const username = (await params).username;
         const user = await User.findOne({ username })
             .select('-password')
             .populate({ path: 'posts' });
