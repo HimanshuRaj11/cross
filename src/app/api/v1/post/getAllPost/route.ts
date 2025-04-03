b9370d4f20c4e63c94f35623c28e5a8ee553b03f
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-04-03 13:07:11 +0530

    commit

--- src/app/api/v1/post/getAllPost/route.ts
+++ src/app/api/v1/post/getAllPost/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 
 export async function GET() {
+    await connectDB()
     try {
         const posts = await Post.find().sort({ createdAt: -1 })
             .populate({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
   const { socket, setSocket } = useGlobalContext()
 
   useEffect(() => {
+
     dispatch(fetchPost() as any)
 
   }, [])
